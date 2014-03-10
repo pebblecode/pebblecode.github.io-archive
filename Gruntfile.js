a7f6c82fc6d6c4d82c320d81a7a7f43e37b0b1d2
Mark Durrant <mark.j.durrant.gmail.com>
2014-03-10 15:25:54 +0000

    add astrazeneca dir for promo videos

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,137 +5,159 @@ module.exports = function ( grunt ) {
 
   // grunt config options
   grunt.initConfig({
-    // read package.json
-    pkg: grunt.file.readJSON( 'package.json' ),
+  // read package.json
+  pkg: grunt.file.readJSON( 'package.json' ),
 
-    // assemble (static site generator) options
-    assemble: {
+  // assemble (static site generator) options
+  assemble: {
+    options: {
+      partials: [ 'src/templates/partials/*.hbs' ],
+      layout: [ 'src/templates/layouts/default.hbs' ],
+      data: [ 'src/data/*.json' ],
+      postprocess: require( 'pretty' ),
+      flatten: true
+    },
+
+    pages: {
+      src: [ 'src/templates/pages/*.hbs' ],
+      dest: 'dist/'
+    },
+
+    astrazeneca: {
+      src: [ 'src/templates/pages/astrazeneca/*.hbs' ],
+      dest: 'dist/astrazeneca/' 
+    }
+  },
+
+  // clean up old html
+  clean: {
+    all: [ 'dist/**/*.html' ]
+  },
+
+  // compile Sass
+  sass: {
+    dist: {
       options: {
-        partials: [ 'src/templates/partials/*.hbs' ],
-        layout: [ 'src/templates/layouts/default.hbs' ],
-        data: [ 'src/data/*.json' ],
-        postprocess: require( 'pretty' ),
-        flatten: true
+        style: 'compressed',
+        lineNumbers: true,
+        sourcemap: true
       },
-
-      pages: {
-        src: [ 'src/templates/pages/*.hbs' ],
-        dest: 'dist/'
+      files: {
+        'dist/css/styles.css': 'src/sass/styles.scss'
       }
-    },
+    }
+  },
 
-    // clean up old html
-    clean: {
-      all: [ 'dist/**/*.html' ]
+  // hint all JS files
+  jshint:{
+    data: {
+      src: [ 'src/data/**/*.json', 'src/data/**/*.json' ]
     },
+    js: {
+      src: [ 'Gruntfile.js', 'src/js/*.js', '!src/js/*.min.js' ]
+    }
+  },
 
-    // compile Sass
-    sass: {
-      dist: {
-        options: {
-          style: 'compressed',
-          lineNumbers: true,
-          sourcemap: true
-        },
-        files: {
-          'dist/css/styles.css': 'src/sass/styles.scss'
-        }
+  uglify:{
+    my_target: {
+      files: {
+        'src/js/main.min.js': [ 'src/js/main.js' ],
+        'src/js/tumblr.min.js': [ 'src/js/tumblr.js' ]
       }
-    },
+    }
+  },
 
-    // hint all JS files
-    jshint:{
-      data: {
-        src: [ 'src/data/**/*.json', 'src/data/**/*.json' ]
-      },
-      js: {
-        src: [ 'Gruntfile.js', 'src/js/*.js', '!src/js/*.min.js' ]
-      }
-    },
-
-    uglify:{
-      my_target: {
-        files: {
-          'src/js/main.min.js': [ 'src/js/main.js' ],
-          'src/js/tumblr.min.js': [ 'src/js/tumblr.js' ]
-        }
-      }
-    },
-
-    // copy files to dist/
-    copy: {
-      // copy javascript
-      scripts: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/js/',
-            src: [ '**/*.min.js' ],
-            dest: 'dist/js/'
-          }
-        ]
-      },
-      // copy images
-      images: {
-        files: [{
+  // copy files to dist/
+  copy: {
+    // copy javascript
+    scripts: {
+      files: [
+        {
           expand: true,
-          cwd: 'src/img/',
-          src: [ '**' ],
-          dest: 'dist/img/'
-        }]
-      }
-    },
-
-    // spin up local dev server
-    connect: {
-      server: {
-        options: {
-          port: 7770,
-          base: 'dist/',
-          hostname: '0.0.0.0'
+          cwd: 'src/js/',
+          src: [ '**/*.min.js' ],
+          dest: 'dist/js/'
         }
-      }
+      ]
+    },
+    // copy images
+    images: {
+      files: [{
+        expand: true,
+        cwd: 'src/img/',
+        src: [ '**' ],
+        dest: 'dist/img/'
+      }]
     },
 
-    // open browser on start
-    open: {
-      dev: {
-        path: 'http://localhost:7770'
-      }
+    robot: {
+      files: [{
+        expand: true,
+        cwd: 'src/',
+        src: [ 'robots.txt' ],
+        dest: 'dist/'
+      }]
     },
 
-    // watch for changes & complete tasks
-    watch: {
+    astrazenecaVideos: {
+      files: [{
+        expand: true,
+        cwd: 'src/astrazeneca-videos/',
+        src: [ '**' ],
+        dest: 'dist/astrazeneca/'
+      }]
+    }
+  },
+
+  // spin up local dev server
+  connect: {
+    server: {
       options: {
-        livereload: true
-      },
-      // watch for changes to templates and data. Remove all html then rebuild
-      html: {
-        files: [ 'src/templates/**/*.hbs', 'src/data/*.json' ],
-        tasks: [ 'clean', 'assemble' ]
-      },
-      // watch for Sass changes. Complie to CSS
-      css: {
-        files: 'src/sass/*.scss',
-        tasks: [ 'sass' ]
-      },
-      // Watch for javascript changes. Run JShint & copy to dist
-      js: {
-        files: [ 'Gruntfile.js', 'src/js/*.js', '!src/js/*.min.js' ],
-        tasks: [ 'jshint', 'uglify', 'copy:scripts']
-      },
-      // watch for changes to images. Copy to dist
-      images: {
-        files: [ 'src/img/**/*' ],
-        tasks: [ 'copy:images' ]
-      },
-    },
-    
-    'gh-pages': {
-      options: {
-        base: 'dist'
-      },
+        port: 7770,
+        base: 'dist/',
+        hostname: '0.0.0.0'
+      }
+    }
+  },
 
+  // open browser on start
+  open: {
+    dev: {
+      path: 'http://localhost:7770'
+    }
+  },
+
+  // watch for changes & complete tasks
+  watch: {
+    options: {
+      livereload: true
+    },
+    // watch for changes to templates and data. Remove all html then rebuild
+    html: {
+      files: [ 'src/templates/**/*.hbs', 'src/data/*.json' ],
+      tasks: [ 'clean', 'assemble' ]
+    },
+    // watch for Sass changes. Complie to CSS
+    css: {
+      files: 'src/sass/*.scss',
+      tasks: [ 'sass' ]
+    },
+    // Watch for javascript changes. Run JShint & copy to dist
+    js: {
+      files: [ 'Gruntfile.js', 'src/js/*.js', '!src/js/*.min.js' ],
+      tasks: [ 'jshint', 'uglify', 'copy:scripts']
+    },
+    // watch for changes to images. Copy to dist
+    images: {
+      files: [ 'src/img/**/*' ],
+      tasks: [ 'copy:images' ]
+    },
+  },
+  
+  'gh-pages': {
+    options: {
+      base: 'dist'
+    },
       src: [ '*.html', 'js/**/*', 'css/**/*', 'img/**/*' ]
     }
   });
@@ -153,5 +175,5 @@ module.exports = function ( grunt ) {
 
   grunt.registerTask( 'default', [ 'connect', 'watch' ] );
 
-  grunt.registerTask( 'make', [ 'clean', 'assemble', 'sass', 'copy:scripts', 'copy:images' ] );
+  grunt.registerTask( 'make', [ 'clean', 'assemble', 'sass', 'copy:scripts', 'copy:images', 'copy:robot', 'copy:astrazenecaVideos' ] );
 };
